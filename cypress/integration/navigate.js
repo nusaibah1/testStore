dfe4c473c4268b8ce6126394720f8cf19bdd93be
Nusaibah1 <charifnusaibah01@gmail.com>
2025-11-06 12:10:21 +0200

    Primary Checks

--- cypress/integration/navigate.js
+++ cypress/integration/navigate.js
@@ -1,1 +1,12 @@
 /// <reference types="cypress"/>
+describe("End to end eccomerce test", () => {
+    it("Submit Order", () => {
+      const productName = "Blackberry"
+      cy.visit("https://rahulshettyacademy.com/loginpagePractise/#/");
+      cy.get('#username').type('rahulshettyacademy')
+      cy.get("#password").type("learning")
+      cy.contains('Sign In').click()
+      cy.contains('Shop Name').should('be.visible')
+      cy.get('app-card').should('have.length', 4)
+  });
+});
